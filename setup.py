6a5cc367c9af14d21426724e069fd67fce746860
DataScrutunizingwithMayur <rane.mayur97@gmail.com>
2024-01-11 21:53:09 -0500

    Added Data

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "chicken-disease-classification"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "DataScrutinizingwithMayur"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "rane.mayur97@gmail.com"
 
 
 setuptools.setup(
